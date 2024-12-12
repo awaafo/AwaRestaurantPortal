322d86f2a126f714e8f331012f7a712ef1fd5e44
awaafo <98856828+awaafo@users.noreply.github.com>
2024-12-12 17:02:52 -0500

    Delete TEMPLATES directory

--- TEMPLATES/home.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head><title>Restaurant Portal</title></head>
-<body>
-    <h1>Restaurant Portal</h1>
-    <a href="index.php?action=addReservation">Add Reservation</a> |
-    <a href="index.php?action=viewReservations">View Reservations</a>
-</body>
-</html>
