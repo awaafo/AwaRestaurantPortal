47a3f0658d01ae99f28a0672a68c744467826587
awaafo <98856828+awaafo@users.noreply.github.com>
2024-12-12 17:13:45 -0500

    Add files via upload

---
+++ TEMPLATE/home.php
@@ -0,0 +1,8 @@
+<html lang="en">
+<head><title>Restaurant Portal</title></head>
+<body>
+    <h1>Awa's Restaurant Portal</h1>
+    <a href="index.php?action=addReservation">Add Reservation</a> |
+    <a href="index.php?action=viewReservations">View Reservations</a>
+</body>
+</html>
